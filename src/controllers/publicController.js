6285494d53f4ddab90a2cdb30f7ba4cb8b898e27
phamtu14 <20020493@vnu.edu.vn>
2024-03-04 22:52:28 +0700

    fix

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -1,13 +1,14 @@
 import { publicService } from '../services/publicService.js';
 import { StatusCodes } from 'http-status-codes';
 import ApiError from '../utils/ApiError.js';
+import { shuffleArray } from '../utils/shuffle.js';
 
 // get all products
 const allProducts = async (req, res, next) => {
     try {
-        const products = await publicService.allProducts();
-        res.status(StatusCodes.OK).json(products);
-        next();
+        let products = await publicService.allProducts();
+        let newProducts = shuffleArray(products);
+        res.status(StatusCodes.OK).json(newProducts);
     } catch (error) {
         next(error);
     }
@@ -16,12 +17,13 @@ const allProducts = async (req, res, next) => {
 // get product by category name
 const categoryProduct = async (req, res, next) => {
     try {
-        const category = req.params.category.slice(1);
+        let category = req.params.category.slice(1);
         if (!category) {
             res.status(StatusCodes.NOT_FOUND).json('Category is not found');
         } else {
             const results = await publicService.categoryProduct(category);
             res.status(StatusCodes.OK).json(results);
+            next();
         }
     } catch (error) {
         next(error);
