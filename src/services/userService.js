2af2d47958c0709b0987e0c51c97dd471e4fb65e
ahntu14 <phamtu559@gmail.com>
2024-04-29 14:55:36 +0700

    a

--- src/services/userService.js
+++ src/services/userService.js
@@ -231,7 +231,7 @@ const DetailOrder = async (userId) => {
             CONCAT(
                 '[',
                 GROUP_CONCAT(
-                    JSON_OBJECT('name', p.name, 'imageUrl', p.imageUrl, 'quantity', od.quantity, 'oldPrice', p.oldPrice, 'newPrice', p.newPrice, 'rating', r.rate)
+                    JSON_OBJECT('id', p.id, 'name', p.name, 'imageUrl', p.imageUrl, 'quantity', od.quantity, 'oldPrice', p.oldPrice, 'newPrice', p.newPrice, 'rating', r.rate)
                     SEPARATOR ','
                 ),
                 ']'
